4bcfefdff7c14b3f4c545b20935758d7ae7faba0
h455257166 <455257166@qq.com>
2021-04-08 08:18:37 +0800

    Update migu.sh

--- migu.sh
+++ migu.sh
@@ -5,25 +5,25 @@ curl -H "Cookie: ${MIGUCOOKIE1}" -X POST 'https://gw.aikan.miguvideo.com/ygw/api
 echo ''
 echo '---------------check sign---------------'
 curl -H "Cookie: ${MIGUCOOKIE1}" -X POST 'https://gw.aikan.miguvideo.com/ygw/api/dispatch/migu-sign/checkSign'
-
+echo ''
 #咪咕账号2
 echo '------------------sign------------------'
 curl -H "Cookie: ${MIGUCOOKIE2}" -X POST 'https://gw.aikan.miguvideo.com/ygw/api/dispatch/migu-sign/sign'
 echo ''
 echo '---------------check sign---------------'
 curl -H "Cookie: ${MIGUCOOKIE2}" -X POST 'https://gw.aikan.miguvideo.com/ygw/api/dispatch/migu-sign/checkSign'
-
+echo ''
 #咪咕账号3
 echo '------------------sign------------------'
 curl -H "Cookie: ${MIGUCOOKIE3}" -X POST 'https://gw.aikan.miguvideo.com/ygw/api/dispatch/migu-sign/sign'
 echo ''
 echo '---------------check sign---------------'
 curl -H "Cookie: ${MIGUCOOKIE3}" -X POST 'https://gw.aikan.miguvideo.com/ygw/api/dispatch/migu-sign/checkSign'
-
+echo ''
 #咪咕账号4
 echo '------------------sign------------------'
 curl -H "Cookie: ${MIGUCOOKIE4}" -X POST 'https://gw.aikan.miguvideo.com/ygw/api/dispatch/migu-sign/sign'
 echo ''
 echo '---------------check sign---------------'
 curl -H "Cookie: ${MIGUCOOKIE4}" -X POST 'https://gw.aikan.miguvideo.com/ygw/api/dispatch/migu-sign/checkSign'
-
+echo ''
